079c610846e1136f0b35de7c5bfc48134e4448eb
David Beck <dbec.pro@gmail.com>
2014-09-23 12:37:14 +0200

    1st push with view & ng controller + upd htaccess

--- application/views/application_view.php
+++ application/views/application_view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-    <head ng-app="brokenstuff">
+<html ng-app="brokenstuff">
+    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>BrokenStuff</title>
@@ -10,7 +11,8 @@
         <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/main.css">
 		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-		<script src="angular/angular.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.5/angular.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.5/angular-route.js"></script>
 		<script src="app.js"></script>
     </head>
     <body>
@@ -18,16 +20,8 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-    <div class="container container-main">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="huge-button"><h2><a href="#"><span class="glyphicon glyphicon-search"></span> &nbsp;Rechercher un incident</h2></a>
-        </div>
-        <div class="huge-button"><h2><a href="#"><span class="glyphicon glyphicon-plus"></span> &nbsp;Rapporter un incident</h2></a>
-        </div>
-      </div>
-
-    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<div ng-view></div>     
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
 
 	<script src="bootstrap/js/bootstrap.min.js"></script>
